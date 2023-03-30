cc1932235cb55a3b7ec403abd301d0a9c7cd7c57
Leonardo Orejuela <leonardo.orejuela@gmail.com>
2023-03-30 13:20:38 -0500

    🔘 Creating button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -2,6 +2,8 @@ import { async } from '@firebase/util';
 import { useState } from 'react';
 import FormInput  from '../form-input/form-input.component';
 
+import Button from '../button/button.component';
+
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'; 
 
 import './sign-up-form.styles.scss';
@@ -59,7 +61,7 @@ const SignUpForm = () => {
                 <FormInput label='Email' type='email' required onChange={handleChange} name='email' value={email}/>
                 <FormInput label='Password' type='password' required onChange={handleChange} name='password' value={password}/>
                 <FormInput label='Confirm Password' type='password' required onChange={handleChange} name='confirmPassword' value={confirmPassword}/>
-                <button type='submit'>Sign Up</button>
+                <Button type='submit'>Sign Up</Button>
             </form>
         </div>
     );
